15863960cb91eb469201127494f3d5ea04530ba1
Douglas Silva <douglasifbaianosb@gmail.com>
2022-08-24 09:26:02 -0300

    Add modalprovider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '../../styles/global.scss';
 import { FormProvider } from '../contexts/FormStepContext';
+import { ModalProvider } from '../contexts/ModalContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <FormProvider>
-      <Component {...pageProps} />
+      <ModalProvider>
+        <Component {...pageProps} />
+      </ModalProvider>
     </FormProvider>
   );
 }
